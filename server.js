b473190e9d093afce8d5e1c335c6ef7f975bb51f
Lalit Rajput <lalit.rajput@jadeglobal.com>
2018-03-27 12:07:07 +0530

    Intial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');  //Setting view engines as hbs or handle bars
+
+
+app.use(express.static(__dirname+'/public')); //takes middle ware functions
+
+app.use((req,res,next)=>{
+
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log',log+'\n');
+    console.log(log); 
+    next()
+});
+
+app.use((req,res,next)=>{
+    res.render('maintaince.hbs');
+});
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('getScreamer',(txt)=>{
+    return txt.toUpperCase();
+})
+app.get('/',(req,res)=>{ //Sending data to client
+    // res.send('<h1>Hello Express!!</h1>'); //sending string to client
+    // res.send({
+    //     name : 'Lalit',
+    //     likes : [
+    //         'Cricket',
+    //         'Books'
+    //     ]
+    // })
+    res.render('home.hbs',{
+        pageTitle : 'Home Page',
+        welcomeNote : 'Welcome Page Text lies here!!! !!!!!'
+    })
+});
+
+app.get('/about',(req,res)=>{     // ROuting the pages
+    res.render('about.hbs',{
+        pageTitle : 'About Page',
+        currentYear : new Date().getFullYear()
+    });
+})
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage :'Unable to handle request          `'
+
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("***Server is running at localhost:3000***")
+});
